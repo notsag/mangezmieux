6a60e15bc60311af449b48d27c6136dd1b38e6b8
mangezmieux <mangezmieux@glenn-liveconsole.(none)>
2012-12-17 15:44:27 +0000

    Password removed from README ;)

--- mangezmieux/urls.py
+++ mangezmieux/urls.py
@@ -5,13 +5,6 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mangezmieux.views.home', name='home'),
-    # url(r'^mangezmieux/', include('mangezmieux.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
