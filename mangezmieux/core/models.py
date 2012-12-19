31e18c4c0af855b8ac8e4953ff589be8e49fcb1e
mangezmieux <mangezmieux@glenn-liveconsole.(none)>
2012-12-19 15:34:54 +0000

    Change in the product_type sequence

--- mangezmieux/core/models.py
+++ mangezmieux/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(primary_key=True, max_length=25)
-    TYPE_CHOICES = ('Viande', 'Poisson', 'Féculent', 'Légume', 'Produit laitier')
+    TYPE_CHOICES = (('V','Viande'),('P','Poisson'),('F','Féculent'),('L','Légume'),('PL','Produit laitier'))
     product_type = models.CharField(max_length=25, choices=TYPE_CHOICES)
 
     def __unicode__(self):
@@ -14,7 +14,7 @@ class RecipeLine(models.Model):
     quantity = models.IntegerField()
     UNIT_CHOICES = (('g', 'Grammes'), ('mL', 'millilitres'), ('càc', 'Cuillère à café'), ('càs', 'Cuillère à soupe'))
     unit = models.CharField(max_length=25, choices=UNIT_CHOICES, null=True)
-    
+
     def __unicode__(self):
         return u'%d %s %s' % (self.quantity, self.unit, self.product)
 
