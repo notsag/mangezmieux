269956daeb87b1092e607aaf97e0630d0c21f63e
vlagneau <vlagneaupro@gmail.com>
2013-02-20 22:42:24 +0100

    Retouches sur la recherche de recettes

--- mangezmieux/recette/views.py
+++ mangezmieux/recette/views.py
@@ -2,11 +2,7 @@
 from django.shortcuts import render
 from core.models import *
 from forms import SearchForm
-from planning.forms import *
-
-def liste(request):
-	recettes = Recette.objects.filter(est_valide=True)
-	return render(request, 'recette/liste.html', locals())
+#from planning.forms import *
 
 def show(request, id):
 	recettes = Recette.objects.filter(pk=id, est_valide=True)
