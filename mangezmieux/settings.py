272154613f74290085fa096b8499d07deeab36df
maxibgoode <maxime.gaston@gmail.com>
2013-02-20 11:06:00 +0100

    Suppression de la pagination dans l'API

--- mangezmieux/settings.py
+++ mangezmieux/settings.py
@@ -100,7 +100,6 @@ REST_FRAMEWORK = {
 		'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
 		'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication','rest_framework.authentication.TokenAuthentication'),
 		'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
-		'PAGINATE_BY': 10
 }
 
 
