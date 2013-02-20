345d2b5d769784176fab29b107474982e54ba954
Alexis <alexis.deberg+git@gmail.com>
2013-02-21 00:39:27 +0100

    Oubli / dans urls recettes

--- mangezmieux/recette/urls.py
+++ mangezmieux/recette/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-	url(r'^recherche$', 'recette.views.recherche'),
+	url(r'^recherche/$', 'recette.views.recherche'),
 	url(r'^detail/(?P<id>\d*)/$', 'recette.views.detail'),
 )
