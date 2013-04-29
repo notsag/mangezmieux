d6fa4c8a7276b065690609fa6c259cfa17c4bbb3
Jean-Christophe Mougamadou <jeanchristophe.mougamadou@gmail.com>
2013-04-29 18:15:12 +0200

    Statistiques nutritives /stats

--- mangezmieux/auth/views.py
+++ mangezmieux/auth/views.py
@@ -7,11 +7,130 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
 from forms import *
+from recette.models import *
+from datetime import *
+from core.models import *
 
 @login_required(login_url='/connexion')
 def stats(request):
+	user = request.user
+    
+	"""
+	    On recupere la date passee en parametre get
+	"""
+	dateS = request.GET.get('d', None)
+	if dateS != None:
+	    try:
+		dateC = parser.parse(dateS).date()
+	    except:
+		dateC = date.today()
+	else:
+	    dateC = date.today()
+	
+	"""
+	    On calcule la date de debut de semaine
+	"""
+	debutSemaine = dateC
+	debutSemaineJour = debutSemaine.strftime('%A')
+	while debutSemaineJour != "Monday":
+	    debutSemaine = debutSemaine + timedelta(days=-1)
+	    debutSemaineJour = debutSemaine.strftime('%A')
+	
+	"""
+	    On calcule la date de fin de semaine
+	"""
+	finSemaine = dateC
+	finSemaineJour = debutSemaine.strftime('%A')
+	while finSemaineJour != "Sunday":
+	    finSemaine = finSemaine + timedelta(days=1)
+	    finSemaineJour = finSemaine.strftime('%A')
+	
+	"""
+	    On verifie si on est dans la semaine courange pour mettre en valeur le jour courant
+	"""
+	if date.today() < finSemaine and date.today() > debutSemaine:
+	    ok = True
+	    day = date.today().strftime('%A')
+	
+	"""
+	    On recupere les repas de la semaine courante
+	"""
+	if user.is_authenticated():
+	    repass = Repas.objects.filter(date__gte = debutSemaine, date__lte = finSemaine, utilisateur = user).order_by('date','ordre')
+	else :
+	    repass = Repas.objects.filter(date__gte = debutSemaine, date__lte = finSemaine).order_by('date','ordre')
+	
+	planning = remplirPlanning(repass, debutSemaine)
+	
+	stats = []
+	for i in xrange(7):
+	    stats.append([])
+	    for j in xrange(5):
+		stats[i].append(0)
+	
+	i = 0
+	for jour in planning:
+		proteine = 0
+		glucide = 0
+		lipide = 0
+		fibre = 0
+		sodium = 0
+		for ordre in jour:
+			if ordre.recette != False :
+				for recette in ordre.recette.all():
+					for ligne in recette.lignes.all():
+						proteine = proteine + ligne.produit.valeur_nutritionnelle.proteines
+						glucide = glucide + ligne.produit.valeur_nutritionnelle.glucides
+						lipide = lipide + ligne.produit.valeur_nutritionnelle.lipides
+						fibre = fibre + ligne.produit.valeur_nutritionnelle.fibres
+						sodium = sodium + ligne.produit.valeur_nutritionnelle.sodium
+			if ordre.recette != False :
+				for ligne in ordre.produit.all():
+					proteine = proteine + ligne.produit.valeur_nutritionnelle.proteines
+					glucide = glucide + ligne.produit.valeur_nutritionnelle.glucides
+					lipide = lipide + ligne.produit.valeur_nutritionnelle.lipides
+					fibre = fibre + ligne.produit.valeur_nutritionnelle.fibres
+					sodium = sodium + ligne.produit.valeur_nutritionnelle.sodium
+		
+		stats[i][0] = proteine
+		stats[i][1] = glucide
+		stats[i][2] = lipide
+		stats[i][3] = fibre
+		stats[i][4] = sodium
+		i = i + 1
 	
 	return render(request, 'auth/stats.html', locals())	
+
+def remplirPlanning(repass, debutSemaine):
+	planning = []
+	for i in xrange(7):
+	    planning.append([])
+	    for j in xrange(3):
+		repasVide = RepasNonPersiste()
+		repasVide.date = debutSemaine + timedelta(days= i)
+		repasVide.ordre = j
+		repasVide.nb_personne = 0
+		repasVide.recette = False
+		repasVide.produit = False
+		planning[i].append(repasVide)
+	
+	for repas in repass :
+	    if repas.date.strftime('%A') == 'Monday':
+		planning[0][repas.ordre] = repas
+	    elif repas.date.strftime('%A') == 'Tuesday':
+		planning[1][repas.ordre] = repas
+	    elif repas.date.strftime('%A') == 'Wednesday':
+		planning[2][repas.ordre] = repas
+	    elif repas.date.strftime('%A') == 'Thursday':
+		planning[3][repas.ordre] = repas
+	    elif repas.date.strftime('%A') == 'Friday':
+		planning[4][repas.ordre] = repas
+	    elif repas.date.strftime('%A') == 'Saturday':
+		planning[5][repas.ordre] = repas
+	    elif repas.date.strftime('%A') == 'Sunday':
+		planning[6][repas.ordre] = repas
+	
+	return planning
 
 def inscription(request):
 	"""
